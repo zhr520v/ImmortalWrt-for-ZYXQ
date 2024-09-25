3eaa0414fc207d0d9b073b2f86507f2fce64deff
zhr520v <63360959+zhr520v@users.noreply.github.com>
2024-09-26 00:01:19 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -7,5 +7,4 @@ sed -i 's/192.168.1.1/192.168.80.2/g' package/base-files/files/bin/config_genera
 rm -rf feeds/luci/applications/luci-app-passwall
 
 # Add packages
-git clone https://github.com/nantayo/My-Pkg package/my-pkg
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
