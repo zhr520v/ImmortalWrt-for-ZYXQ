dfb40338f426a1b9de8997fdf073fcc3608f24ec
zhr520v <63360959+zhr520v@users.noreply.github.com>
2024-09-27 22:03:10 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -3,8 +3,5 @@
 # Default IP
 sed -i 's/192.168.1.1/192.168.80.2/g' package/base-files/files/bin/config_generate
 
-# Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
-
 # Add packages
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
