619af95251bf3274e8d726834173a38968fdea20
zhr520v <63360959+zhr520v@users.noreply.github.com>
2024-12-31 14:02:41 +0800

    Update diy.sh

--- ZYXQ/diy.sh
+++ ZYXQ/diy.sh
@@ -5,3 +5,7 @@ sed -i 's/192.168.1.1/192.168.80.2/g' package/base-files/files/bin/config_genera
 
 # Add packages
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+
+#添加自定义软件源
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
