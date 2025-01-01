c901ef7b30ca7d9db1687c8ae859d6288a4f49d9
zhr520v <63360959+zhr520v@users.noreply.github.com>
2025-01-01 15:08:43 +0800

    Update diy.sh

--- ZYXQ/diy.sh
+++ ZYXQ/diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.80.2/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.80.2/g' package/base-files/files/bin/config_generate
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
